9f8b04942e9e3a98e16600a9de5db22f8bf3417e
takanbi <tunji.akanbi@century.edu>
2023-04-12 13:34:47 -0500

    added checking section, added reLoadChecking function with some stagger with GSAP

--- mobileBankingApp/js/app.js
+++ mobileBankingApp/js/app.js
@@ -2,17 +2,20 @@ $(function () {
   let login = $("section#login");
   let welcomeMain = $("section#welcome-main-content");
   let fullGradient = $("div#full-gradient-background");
-  let submit = $("button#submit");
+  let submitBtn = $("button#submit");
   let welcomeTop = $("#welcome-top-content");
   let welcomeMiddle = $("#welcome-middle-content");
   let welcomeBottom = $("#welcome-bottom-content");
+  let checkingMain = $("#checking-main-content");
+  let checkingBtn = $("#checking");
+  let redChevron = $("#left");
+  let checkingDataDivs = $(".checking-data-divs");
+  console.log(checkingDataDivs);
 
   login.css("visibility", "visible");
+  checkingMain.css("visibility", "hidden");
   welcomeMain.css("visibility", "hidden");
-
-  // let welcomeMidChildren = welcomeMiddle.children();
-  // console.log(welcomeMidChildren);
-  submit.click(loadWelcome);
+  submitBtn.click(loadWelcome);
 
   function loadWelcome() {
     login.css("visibility", "hidden");
@@ -22,8 +25,17 @@ $(function () {
     tl.from(welcomeTop,{opacity: 0, y: -200, duration: 0.8, ease: "elastic.out(1, 0.75)"},"+=0.5");
     tl.from(welcomeMiddle,{opacity: 0, y: 200, duration: 0.8, ease: "elastic.out(1, 0.75)"});
   }
-  $("#checking").click(loadChecking);
+  checkingBtn.click(loadChecking);
   function loadChecking() {
-    welcomeTop.css("visibility", "hidden");
+    welcomeMain.css("visibility", "hidden");
+    checkingMain.css("visibility", "visible");
+    let tl = gsap.timeline();
+    tl.from(checkingDataDivs, {opacity: 0, duration: 2, stagger: 0.3, ease: "power4.out"});
+
+  }
+redChevron.click(reLoadchecking);
+  function reLoadchecking(){
+    checkingMain.css("visibility", "hidden");
+    welcomeMain.css("visibility", "visible");
   }
 });
