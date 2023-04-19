8cc736a95f440581f0733c9729eead2360dccd6f
takanbi <tunji.akanbi@century.edu>
2023-04-19 13:29:53 -0500

    added more GSAP animation to mobile banking app

--- mobileBankingApp/js/app.js
+++ mobileBankingApp/js/app.js
@@ -1,4 +1,7 @@
 $(function () {
+  gsap.registerPlugin(GSDevTools, DrawSVGPlugin);
+  let container = $(".container");
+  console.log(container);
   let login = $("section#login");
   let welcomeMain = $("section#welcome-main-content");
   let fullGradient = $("div#full-gradient-background");
@@ -10,7 +13,20 @@ $(function () {
   let checkingBtn = $("#checking");
   let redChevron = $("#left");
   let checkingDataDivs = $(".checking-data-divs");
-  console.log(checkingDataDivs);
+  // console.log(checkingDataDivs);
+
+  function loadLogin() {
+    let tl = gsap.timeline({id: "tl00"});
+    // GSDevTools.create({animation: "tl00"});
+    tl.fromTo(container, {opacity: 0 }, {opacity: 1, duration: 1.5});
+    tl.from($(".st1"), {fill: "transparent", duration:1.5});
+    tl.from($(".st1"), {drawSVG: "0%", duration:1.5}, "-=0.75");
+    tl.from($(".st3"), {opacity: 0, duration: 1, stagger: 0.1, ease: "power4.out"},"-=1.25");
+    tl.from($("input#username"), {opacity: 0, y: 100, duration:0.75},"-=0.25");
+    tl.from($("input#password"), {opacity: 0, y: 100, duration:0.75},"-=0.25");
+    tl.fromTo($("button#submit"), {opacity: 0, y: 100, }, {opacity:1, y: 0, duration:0.75},"-=0.25");
+  };
+  loadLogin();
 
   login.css("visibility", "visible");
   checkingMain.css("visibility", "hidden");
@@ -21,9 +37,10 @@ $(function () {
     login.css("visibility", "hidden");
     welcomeMain.css("visibility", "visible");
     fullGradient.css("visibility", "hidden");
-    let tl = gsap.timeline();
+    let tl = gsap.timeline({id:"tl01"});
     tl.from(welcomeTop,{opacity: 0, y: -200, duration: 0.8, ease: "elastic.out(1, 0.75)"},"+=0.5");
     tl.from(welcomeMiddle,{opacity: 0, y: 200, duration: 0.8, ease: "elastic.out(1, 0.75)"});
+    // GSDevTools.create({animation: "tl01"});
   }
   checkingBtn.click(loadChecking);
   function loadChecking() {
